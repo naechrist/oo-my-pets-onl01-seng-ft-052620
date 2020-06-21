0b40dd46d0a8826423cfdb49b40d711fce9a72a3
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-21 23:57:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,3 @@
-# class Dog
-# attr_accessor :owner, :mood
-# attr_reader :name 
-# @@all = []
-
-#   def initialize(name, owner)
-#     @name = name 
-#     @owner = owner
-#     @mood = "nervous"
-#     @@all << self
-#   end
-  
-#   def self.all 
-#     @@all
-#   end
-# end
 class Dog
   attr_accessor :owner, :mood
   attr_reader :name
