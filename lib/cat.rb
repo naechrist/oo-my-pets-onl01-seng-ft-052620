f13414522d95bd5dc2ac4fac658942ebf526e57a
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-21 23:57:31 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,19 +1,3 @@
-# class Cat
-#   attr_accessor :mood, :owner
-#   attr_reader :name
-#   @@all = []
-   
-#   def initialize(name, owner)
-#     @name = name 
-#     @owner = owner
-#     @mood = "nervous"
-#     @@all << self
-#   end
-   
-#   def self.all 
-#     @@all
-#   end
-# end
 class Cat
   attr_accessor :owner, :mood
   attr_reader :name
